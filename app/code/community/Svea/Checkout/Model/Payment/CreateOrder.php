edd4574213f271290e2dce2797a43525bfa278c1
iesus <iesus@webbhuset.se>
2017-12-15 14:10:04 +0000

    get customer reference from customer not root-data

--- app/code/community/Svea/Checkout/Model/Payment/CreateOrder.php
+++ app/code/community/Svea/Checkout/Model/Payment/CreateOrder.php
@@ -115,8 +115,8 @@ class Svea_Checkout_Model_Payment_CreateOrder
         $billingAddress   = new Varien_Object($data->getData('BillingAddress'));
         $shippingAddress  = new Varien_Object($data->getData('ShippingAddress'));
         $customer         = new Varien_Object($data->getData('Customer'));
-        $reference        = ($data->getData('CustomerReference'))
-                          ? ($data->getData('CustomerReference'))
+        $reference        = ($customer->getData('CustomerReference'))
+                          ? ($customer->getData('CustomerReference'))
                           : false;
         $billingFirstname = ($billingAddress->getData('FirstName'))
                           ? $billingAddress->getData('FirstName')
